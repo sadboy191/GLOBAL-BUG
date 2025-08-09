8415c3ee78644c705544922a1b3ca9f3728f5883
sadboy191 <lopezben468@gmail.com>
2025-08-09 20:02:00 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
-global.namaown = "Qasim Ali" // your owner name here
+global.namaown = "♜ꦽ⃡⃝  𓆩MVP𓆪 ꦽ⃡⃝♜" // your owner name here
 global.namabot = "© GlobalTechInfo" // your bot name here
 global.versisc = "9.5.0" //
-global.owner = ["923444844060"] // your owner number here
+global.owner = ["2250100282598"] // your owner number here
 global.tele = "https://t.me/GlobalBotInc" // your telegram here
 global.url = "https://www.youtube.com/@GlobalTechInfo" // Your YouTube Here
 global.namastore = "LynnZxD" // UBAH NAMA STORE LU
-global.simbol = "✪" // You Can Change Simbol
+global.simbol = "!" // You Can Change Simbol
 global.wlcm = []
 global.wlcmm = []
 global.limitawal = {
